77ea41e3900038d86f11d86f6d38c8c58cf8bb31
ceimos <86879499+ceimos@users.noreply.github.com>
2025-03-24 22:34:30 +0530

    Fix : color of cow status

--- app/(app)/(drawer)/(tabs)/index.tsx
+++ app/(app)/(drawer)/(tabs)/index.tsx
@@ -1158,6 +1158,7 @@ const styles = StyleSheet.create({
   detailsValue: {
     fontSize: 14,
     fontWeight: '500',
+    color: '#000', // Black text color for darker appearance
   },
   detailsActions: {
     marginTop: 8,
